5b0c086054395975faac0736a0f449ab6d938878
Radeon <r@q.w>
2021-01-02 14:54:52 +0500

    progress

--- ce301.py
+++ ce301.py
@@ -1,6 +1,8 @@
 import socket
 import sys
 import time
+import serial
+import re
 
 StartSymbol = bytes('/','ascii')
 StopSymbol  = bytes('!','ascii')
@@ -26,189 +28,209 @@ def baud_decode(x):
         'C': 2400,
         'D': 4800,
         'E': 9600,
-    }.get(x, 300)    # 9 is default if x not found
+    }.get(x, 300)  
 
 
 def calc_BCC(data):
-    sum = 0;
+    sum = 0
     for byte in data:
-        sum+=byte;
-        sum = sum & 0xFF;
-    return sum & 0x7f;
+        sum+=byte
+        sum = sum & 0xFF
+    return sum & 0x7f
+
+def parse_param_array(str):
+    params = []
+    lines = str.splitlines()
+    for line in lines:
+        params.append(line[line.find('(')+1:line.rfind(')')])
+    return params
+    
+def parse_name_param_array(str):
+    params = []
+    lines = str.splitlines()
+    prev_name  = ''
+    for line in lines:
+        par = line[line.find('(')+1:line.rfind(')')]
+        name = line[:line.find('(')]
+        if not name:
+            name = prev_name
+        else:
+            prev_name = name
+            
+        params.append([name,par])
+    return params
 
 class CE301:
+    baudrate = None
+    def __init__(self, transport):
+        self.transport = transport
+        
     def init_session(self, address = None):
         self.transport.send(StartSymbol)
         self.transport.send(ReqSymbol)
         if address is not None:
-            self.transport.send(address.encode());
-        self.transport.send(StopSymbol);
-        self.transport.send(EndMsg);
-        null,null,baudrate = self.recv_id();
-        self.program_data(baudrate);
-        
-        rcvd_data,hdr = self.recv_data_block();
-        print("Rcvd: " + rcvd_data + "Hdr " + hdr);
-        self.send_password("777777");
-        
+            self.transport.send(address.encode())
+        self.transport.send(StopSymbol)
+        self.transport.send(EndMsg)
+        vendor,id = self.recv_id()
+        return vendor,id
+
     def general_read(self):
-        self.read_data(baudrate);
-        data_read = self.recv_data_block();
-        stop_s = data_read.find(StopSymbol);
-        print(data_read[:stop_s].decode());
-            
-    def __init__(self, transport):
-        self.transport = transport
-        
+        self.read_data()
+        data, hdr = self.recv_data_block()
+        data = data.rstrip((EndMsg + StopSymbol).decode())
+        return parse_name_param_array(data)
+
+    def program_mode(self):
+        self.program_data()
+        rcvd_data, hdr = self.recv_data_block()
+        addr = rcvd_data.lstrip('(').rstrip(')');
+        return addr;
+          
     def recv_id(self):
-        id_msg = self.transport.recv_end(EndMsg);
-        print (id_msg.decode(errors='replace'));
+        id_msg = self.transport.recv_end(EndMsg)
         start_pos = id_msg.find(StartSymbol)
-        if(start_pos == -1):
-            return None;
-        vendor = id_msg[start_pos+1:start_pos+4].decode();
-        baudrate = id_msg[start_pos+4:start_pos+5].decode();
-        id = id_msg[start_pos+5:].decode();
-        
-        print ("vendor: " + vendor);
-        print ("baudrate: " + baudrate);
-        print ("id: " + id );
-        return vendor, id, baudrate;
+        if start_pos == -1:
+            return None
+        vendor = id_msg[start_pos+1:start_pos+4].decode()
+        self.baudrate = id_msg[start_pos+4:start_pos+5].decode()
+        id = id_msg[start_pos+5:].decode()
+
+        return vendor, id
     
-    def send_password(self,password):
-        self.transport.send(SOHSymbol);
-        data_block= bytes('P1', 'ascii');
-        data_block+= STXSymbol;
-        data_block+= bytes('('+password+')', 'ascii');
-        data_block+= ETXSymbol;
-        BCC = calc_BCC(data_block);
-        self.transport.send(data_block);
-        self.transport.send(bytes([BCC]));
+    def authorize(self, password):
+        self.transport.send(SOHSymbol)
+        data_block= bytes('P1', 'ascii')
+        data_block+= STXSymbol
+        data_block+= bytes('('+password+')', 'ascii')
+        data_block+= ETXSymbol
+        bcc = calc_BCC(data_block)
+        self.transport.send(data_block)
+        self.transport.send(bytes([bcc]))
         
-        rcvd_data = self.transport.rcv(1);
+        rcvd_data = self.transport.rcv(1)
         if rcvd_data == AckSymbol:
             print("Password accepted")
         else:
-            print("password rejected: " + rcvd_data.hex());
+            print("password rejected: " + rcvd_data.hex())
     
     def read_param(self,par_name):
-        self.transport.send(SOHSymbol);
-        data_block= bytes('R1', 'ascii');
-        data_block+= STXSymbol;
-        data_block+= bytes(par_name+'()', 'ascii');
-        data_block+= ETXSymbol;
-        BCC = calc_BCC(data_block);
-        self.transport.send(data_block);
-        self.transport.send(bytes([BCC]));
+        self.transport.send(SOHSymbol)
+        data_block= bytes('R1', 'ascii')
+        data_block+= STXSymbol
+        data_block+= bytes(par_name+'()', 'ascii')
+        data_block+= ETXSymbol
+        bcc = calc_BCC(data_block)
+        self.transport.send(data_block)
+        self.transport.send(bytes([bcc]))
         
-        rcvd_data, hdr = self.recv_data_block();
-        print("Rcvd: " + rcvd_data);
-        return rcvd_data;
+        rcvd_data, hdr = self.recv_data_block()
+        
+        return parse_param_array(rcvd_data)
         
     def end_session(self):
-        self.transport.send(SOHSymbol);
-        data_block= bytes('B0', 'ascii');
-        data_block+= ETXSymbol;
-        BCC = calc_BCC(data_block);
-        self.transport.send(data_block);
-        self.transport.send(bytes([BCC]));
-        
+        self.transport.send(SOHSymbol)
+        data_block= bytes('B0', 'ascii')
+        data_block+= ETXSymbol
+        bcc = calc_BCC(data_block)
+        self.transport.send(data_block)
+        self.transport.send(bytes([bcc]))
+        time.sleep(2)
+                      
     def recv_data_block(self):
-        block = self.transport.recv_end(ETXSymbol);
-        BCC   = self.transport.rcv(1);
-        
-        hdr_start = block.find(SOHSymbol);
+        block = self.transport.recv_end(ETXSymbol)
+        bcc   = self.transport.rcv(1)
+
+        hdr_start = block.find(SOHSymbol)
         header = None
         
-        msg_start = block.find(STXSymbol);
-        
+        msg_start = block.find(STXSymbol)
+
         if(hdr_start >=0):
-            header = block[hdr_start+1:msg_start].decode();
+            header = block[hdr_start+1:msg_start].decode()
         else:
-            hdr_start = msg_start;
-            
+            hdr_start = msg_start
+
         bytes_for_bcc = block[hdr_start+1:]+ETXSymbol
         
-        calculated_BCC = calc_BCC(bytes_for_bcc);
+        calculated_BCC = calc_BCC(bytes_for_bcc)
         
         #todo: check BCC
         
         return block[msg_start+1:].decode(),header
         
         
-    def read_data(self,baud):
+    def read_data(self):
         self.transport.send(AckSymbol)
         self.transport.send(bytes('0', 'ascii')) #normal mode
-        self.transport.send(bytes(baud,'ascii'))
+        self.transport.send(bytes(self.baudrate,'ascii'))
         self.transport.send(bytes('0', 'ascii')) #read mode
         self.transport.send(EndMsg)
     
-    def program_data(self,baud):
+    def program_data(self):
         self.transport.send(AckSymbol)
         self.transport.send(bytes('0', 'ascii')) #normal mode
-        self.transport.send(bytes(baud,'ascii'))
+        self.transport.send(bytes(self.baudrate,'ascii'))
         self.transport.send(bytes('1', 'ascii')) #program mode
         self.transport.send(EndMsg)
 
 
-def parity_kr(n):
+def parity_calc(n):
     parity = 0
     while n: 
         parity = ~parity 
         n = n & (n - 1) 
     return parity 
 
-class CE_transport:
-    remains = None;
-    def __init__(self,address,port):
+class TCP_transport:
+    remains = None
+    softparity = True
+    def __init__(self,address,port,emulateparity = True):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((address,port))
         self.sock.settimeout(10.0)
+        self.softparity = emulateparity
     
     def send(self, bytes_snd):
-        #print("Sending")
-        #print(bytes_snd.hex())
-        paritybytes = bytearray() 
-        for byte in bytes_snd:
-            parity = parity_kr(byte)
-            if(parity != 0):
-                byte = byte | 0x80;
-            paritybytes.append(byte)
-        #print("As")
-        #print(paritybytes.hex())
-        self.sock.sendall(paritybytes)
+        if self.softparity == True:
+            paritybytes = bytearray()
+            for byte in bytes_snd:
+                parity = parity_calc(byte)
+                if(parity != 0):
+                    byte = byte | 0x80
+                paritybytes.append(byte)
+            self.sock.sendall(paritybytes)
+        else:
+            self.sock.sendall(bytes_snd)
     
     def rcv(self, maxsize = 255):
-            data_noparity = bytearray();
-            if self.remains is not None:
-                if len(self.remains) == maxsize:
-                    data_noparity = self.remains;
-                    self.remains = None;
-                    #print("remains exactly")
-                    return data_noparity;
-                elif len(self.remains) < maxsize:
-                    #print("remains less")
-                    data_noparity += self.remains;
-                else:
-                    #print("remains more")
-                    data_noparity = self.remains[:maxsize]
-                    remains = self.remains[maxsize:];
-                    return data_noparity;
-             
-            data = self.sock.recv(maxsize)
-            
-            for byte in data:
-                byte = byte & 0x7f
-                data_noparity.append(byte)
-            return data_noparity;
-            
+        data_noparity = bytearray()
+        if self.remains is not None:
+            if len(self.remains) == maxsize:
+                data_noparity = self.remains
+                self.remains = None
+                return data_noparity
+            elif len(self.remains) < maxsize:
+                data_noparity += self.remains
+            else:
+                data_noparity = self.remains[:maxsize]
+                remains = self.remains[maxsize:]
+                return data_noparity
+
+        data = self.sock.recv(maxsize)
+
+        for byte in data:
+            byte = byte & 0x7f
+            data_noparity.append(byte)
+        return data_noparity
+
     def recv_end(self, End):
-        total_data=bytearray();
+        total_data=bytearray()
         while True:
-                data_noparity = self.rcv();
-                
+                data_noparity = self.rcv()
+
                 if End in data_noparity:
-                    endplace = data_noparity.find(End);
+                    endplace = data_noparity.find(End)
                     #print (endplace);
                     #print (len(data_noparity));
                     if endplace+len(End) < len(data_noparity):
@@ -231,7 +253,77 @@ class CE_transport:
             
         return total_data
         
-        
-    
-    
+class Serial_transport:
+    remains = None
+    softparity = False
+    def __init__(self,port,baudrate,use8bits=False):
+        if use8bits == False:
+            #Open serial in 7-E-1 mode
+            self.softparity = False;
+            self.serial_port = serial.Serial(port, baudrate, timeout=10.0,
+                        parity=serial.PARITY_EVEN, bytesize=serial.SEVENBITS )
+        else:
+            #open in 8-N-1 mode and calculate parity in software
+            self.softparity = True
+            self.serial_port = serial.Serial(port, baudrate, timeout=10.0,
+                                     parity=serial.PARITY_NONE, bytesize=serial.EIGHTBITS)
+    def send(self, bytes_snd):
+        if self.softparity == True:
+            paritybytes = bytearray()
+            for byte in bytes_snd:
+                parity = parity_calc(byte)
+                if (parity != 0):
+                    byte = byte | 0x80
+                paritybytes.append(byte)
+            self.serial_port.write(paritybytes)
+        else:
+            self.serial_port.write(bytes_snd)
+
+    def rcv(self, maxsize=255):
+        data_noparity = bytearray()
+        if self.remains is not None:
+            if len(self.remains) == maxsize:
+                data_noparity = self.remains
+                self.remains = None
+                # print("remains exactly")
+                return data_noparity
+            elif len(self.remains) < maxsize:
+                # print("remains less")
+                data_noparity += self.remains
+            else:
+                # print("remains more")
+                data_noparity = self.remains[:maxsize]
+                remains = self.remains[maxsize:]
+                return data_noparity
+
+        data = self.serial_port.read(maxsize)
+
+        for byte in data:
+            byte = byte & 0x7f
+            data_noparity.append(byte)
+        return data_noparity
+
+    def recv_end(self, End):
+        total_data = bytearray()
+        while True:
+            data_noparity = self.rcv()
+
+            if End in data_noparity:
+                endplace = data_noparity.find(End)
+                if endplace + len(End) < len(data_noparity):
+                    self.remains = data_noparity[endplace + len(End):]
+                total_data += (data_noparity[:data_noparity.find(End)])
+                break
+            total_data += (data_noparity)
+
+            if len(total_data) > 1:
+                if End in total_data:
+                    endplace = total_data.find(End)
+                    if endplace + len(End) < len(total_data):
+                        self.remains = total_data[endplace + len(End):]
+                    return total_data[:endplace]
+                    break
+
+        return total_data
+
     
--- client.py
+++ client.py
@@ -1,16 +1,44 @@
 from ce301 import CE301
-from ce301 import CE_transport
+from ce301 import TCP_transport
+from ce301 import Serial_transport
 
-transport = CE_transport('192.168.7.3',12345)
+transport = TCP_transport('192.168.7.3', 12345)
+#transport = Serial_transport('COM3',9600,use8bits=True)
 
 mymeter = CE301(transport)
 
-print("Hello py!")
+vendor, model = mymeter.init_session('155390511')
+print (vendor + ' ' + model)
 
-mymeter.init_session()
-mymeter.read_param('ET0PE')
-mymeter.read_param('VOLTA')
-mymeter.read_param('CURRE')
-mymeter.read_param('POWEP')
-mymeter.read_param('POWPP')
+data = mymeter.general_read()
+print (data)
 mymeter.end_session()
+
+
+
+vendor, model = mymeter.init_session('155390511')
+print (vendor + ' ' + model)
+
+addr = mymeter.program_mode()
+print(addr)
+
+#active_energy = mymeter.read_param('ET0PE')
+# voltages = mymeter.read_param('VOLTA')
+# currents = mymeter.read_param('CURRE')
+# total_power = mymeter.read_param('POWEP')
+# phase_powers = mymeter.read_param('POWPP')
+# power_factor = mymeter.read_param('COS_f')
+# max_power = mymeter.read_param('CMAPE')
+# m_time = mymeter.read_param('TIME_')
+# m_date = mymeter.read_param('DATE_')
+#mymeter.end_session()
+
+#print (active_energy)
+# print (voltages)
+# print (currents)
+# print (phase_powers)
+# print (total_power)
+# print (power_factor)
+# print (max_power)
+# print (m_time)
+# print (m_date)
